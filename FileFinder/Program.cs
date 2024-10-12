850ec665d3e744117c415c3a5d2b348ec5c27a46
Giuseppe <albanog00@gmail.com>
2024-10-12 15:01:43 +0200

    cleanup

--- FileFinder/Program.cs
+++ FileFinder/Program.cs
@@ -1,28 +1,35 @@
-﻿using CSharpTui.Core.Prompts;
+﻿using Cocona;
+using CSharpTui.Prompts;
 using FileFinder.Core;
-using Cocona;
 
-CoconaLiteApp.Run(async (
-    [Argument] string? fileName,
-    [Option('p')] string? searchPath,
-    [Option('e')] string? extension,
-    bool showErrors,
-    bool exact
+CoconaLiteApp.Run(
+    (
+        [Argument] string? fileName,
+        [Option('p')] string? searchPath,
+        [Option('e')] string? extension,
+        bool showErrors,
+        bool exact
     ) =>
-{
-    var fileExplorer = new FileExplorer(
-        fileName, extension, searchPath, showErrors, exact, null);
-    var prompt = new SelectionPrompt<string>();
-    var tokenSource = new CancellationTokenSource();
-
-    var task = Task.Run(async () =>
     {
-        await foreach (var paths in fileExplorer.FindAsync(tokenSource))
-            prompt.AddChoices(paths);
-    });
+        var fileExplorer = new FileExplorer(
+            fileName,
+            extension,
+            searchPath,
+            showErrors,
+            exact,
+            null
+        );
+        var prompt = new SelectionPrompt<string>();
+        var tokenSource = new CancellationTokenSource();
 
-    var selectedPath = await prompt.ShowAsync("Search results", tokenSource);
+        var task = Task.Run(async () =>
+        {
+            await foreach (var paths in fileExplorer.FindAsync(tokenSource))
+                prompt.AddChoices(paths);
+        });
 
-    Console.WriteLine(selectedPath);
-});
+        var selectedPath = prompt.Show("Search results");
 
+        Console.WriteLine(selectedPath);
+    }
+);
